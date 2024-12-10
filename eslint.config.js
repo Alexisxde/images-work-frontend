259c8a86b4b9d8a2607de543d772d4c3fae6b0cf
Alexisxde <olivarezalexis749@gmail.com>
2024-12-10 01:09:09 -0300

    feat first commit 🎉

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ],
+      'no-console': 'error',
+      'no-label-var': 'error',
+      'no-multi-str': 'error',
+      'no-negated-condition': 'error',
+      'no-nested-ternary': 'error',
+      'no-unneeded-ternary': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error'
+    }
+  }
+)
---
+++ src/components/Test.tsx
@@ -0,0 +1,3 @@
+export default function Test() {
+  return <div className="bg-green-500">Test</div>
+}
